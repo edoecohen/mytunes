b590d7f800add29f84b4b7e1a13b2df8488330f4
edoecohen <edoecohen@gmail.com>
2015-04-09 11:19:52 -0700

    passes all tests

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,13 +1,27 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
+// MVC - View
+// Model - Collection SongQueue
+
 var SongQueueView = Backbone.View.extend({
 
   tagName: "table",
 
   initialize: function() {
+    this.render();
+
+    this.listenTo(this.collection, 'add', function(){
+      this.render();
+    });
   },
 
   render: function() {
-    return this.$el;
+    this.$el.children().detach();
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({ model: song}).render();
+      })
+    );
+
   }
 
 });
