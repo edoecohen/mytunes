f8429dd3367eab27be6d46b4868d609de36c1eb1
edoecohen <edoecohen@gmail.com>
2015-04-09 10:27:05 -0700

    Cleans up repo after merge

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -4,26 +4,10 @@ var SongQueueView = Backbone.View.extend({
   tagName: "table",
 
   initialize: function() {
-      this.render();
-  },
-
-  addSong: function(song) {
-    this.collection.add(song);
-    console.log(this.collection);
-    //this.add(song);
-    // this.model.add("aasfas")
-    // console.log("TEST")
-    this.render();
   },
 
   render: function() {
-    this.$el.children().detach();
-
-    this.$el.html('<th class="queueList">Queue</th>').append(
-      this.collection.map(function(song){
-        return new SongQueueEntryView({model: song}).render();
-      })
-    );
+    return this.$el;
   }
 
 });
